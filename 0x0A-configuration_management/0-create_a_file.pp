f0254cb699dae687a7a9aba2b71311cf3bc14e43
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-06-29 01:21:59 +0300

    tst one

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # creates a file named school
 
 file { '/tmp/school':
-  mode    => '0744'
-  owner   => 'www-data'
-  group  => 'www-data'
-  content => 'I love Puppet'
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
