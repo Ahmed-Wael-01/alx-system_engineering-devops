7ba355133a33e0475c09a3f39b4b25ff70d58c3a
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-08-14 15:00:24 +0300

    original code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,26 +3,32 @@
 number of subscribers for a given subreddit
 """
 
-from requests import get
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
-
+    base_url = 'https://www.reddit.com/'
+    data = {'grant_type': 'password',
+            'username': 'Hot_Seat_8723',
+            'password': '252003ahmed'}
+    auth = requests.auth.HTTPBasicAuth('iCqNlw7qgaMnkYySRxoVkA',
+                                        'k7IeJGPZkGEG2S317NDgKJs4fnm26A')
+    res = requests.post(base_url + 'api/v1/access_token',
+                        data=data,
+                        headers={'user-agent': 'k-fetcher by Hot_Seat_8723'},
+                        auth=auth)
+    dic = res.json()
+    token = 'bearer ' + dic['access_token']
+    base_url = 'https://oauth.reddit.com/'
+    headers = {'Authorization': token,
+                'User-Agent': 'k-fetcher by Hot_Seat_8723'}
+    payload = {'q': subreddit, 'limit': 1, 'sort': 'relevance'}
+    response = requests.get(base_url + '/subreddits/search',
+                            headers=headers,
+                            params=payload)
     try:
-        if results.get('data').get('subscribers') is not None:
-            return results.get('data').get('subscribers')
-        return 0
-    except Exception:
+        response = response.json().get('data')
+        response = response.get('children')[0].get('data').get('subscribers')
+        return response
+    except Exception as e:
         return 0
